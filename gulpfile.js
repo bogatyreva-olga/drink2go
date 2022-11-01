bb027cd9b39299b0d56d02d846465615b28e0ee2
o.bogatyreva <bogatireva.o.v@gmail.com>
2022-11-01 11:27:26 +0300

    add font to project

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,20 @@ import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 import csso from 'gulp-csso';
+import squoosh from 'gulp-libsquoosh';
+import svgstore from 'gulp-svgstore';
+import svgo from 'gulp-svgmin';
+import rename from 'gulp-rename';
+
+export const sprite = () => {
+    return gulp.src('source/images/icons/*.svg')
+        .pipe(svgo())
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename('sprite.svg'))
+        .pipe(gulp.dest('build/images'))
+}
 
 export const styles = () => {
     return gulp.src('source/less/style.less', {sourcemaps: true})
@@ -18,6 +32,27 @@ export const styles = () => {
         .pipe(browser.stream());
 }
 
+export const html = () => {
+    return gulp.src('source/*.html')
+        .pipe(gulp.dest('build'))
+}
+
+export const images = () => {
+    return gulp.src('source/images/**')
+        .pipe(squoosh())
+        .pipe(gulp.dest('build/images'))
+}
+
+export const js = () => {
+    return gulp.src('source/js/**')
+        .pipe(gulp.dest('build/js'))
+}
+
+export const fonts = () => {
+    return gulp.src('source/fonts/**')
+        .pipe(gulp.dest('build/fonts'))
+}
+
 export default gulp.series(
-    styles
+    styles, html, js, images, sprite, fonts
 );
